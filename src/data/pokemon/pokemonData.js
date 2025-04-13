ece402d18b884ac67e39adf29f9eb7cc7b86e420
aditpraditia <adit.praditia@quatronema.co.id>
2025-04-13 08:56:22 +0700

    feat: add consume data pokemon

---
+++ src/data/pokemon/pokemonData.js
@@ -0,0 +1,44 @@
+const pokemonData = [
+    {
+        id: 1001,
+        name: "Bulbasaur",
+        type: "Grass",
+        attack: 49,
+        defense: 49,
+        currentHealth: 450,
+        maxHealth: 600,
+        image: "/assets/images/bulbasaur_pokemon.png",
+    },
+    {
+        id: 1002,
+        name: "Charmander",
+        type: "Fire",
+        attack: 52,
+        defense: 43,
+        currentHealth: 390,
+        maxHealth: 500,
+        image: "/assets/images/kabuto_pokemon.png",
+    },
+    {
+        id: 1003,
+        name: "Squirtle",
+        type: "Water",
+        attack: 48,
+        defense: 65,
+        currentHealth: 750,
+        maxHealth: 1000,
+        image: "/assets/images/bulbasaur_pokemon.png",
+    },
+    {
+        id: 1004,
+        name: "Pikachu",
+        type: "Electric",
+        attack: 55,
+        defense: 40,
+        currentHealth: 350,
+        maxHealth: 880,
+        image: "/assets/images/kabuto_pokemon.png",
+    },
+];
+
+export default pokemonData;
