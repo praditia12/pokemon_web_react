ddfa4acad3abac4ce806199dfa9a3a47717795e4
praditia12 <aditpraditia714@gmail.com>
2025-04-21 23:47:12 +0700

    feat: update App.jsx, wrap Routes use PokemonProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.jsx";
 import DetailPage from "./pages/DetailPage/DetailPage.jsx";
+import { PokemonProvider } from "./components/PokemonContext.jsx";
 
 function App() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/detail/:id" element={<DetailPage />} />
-            </Routes>
+            <PokemonProvider>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/detail/:id" element={<DetailPage />} />
+                </Routes>
+            </PokemonProvider>
         </Router>
     );
 }
